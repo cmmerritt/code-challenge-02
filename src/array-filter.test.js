fc0385a1579b6281aeeb97af6384283a2a3523df
cmmerritt <christiane.merritt@gmail.com>
2021-04-27 22:17:21 -0700

    pass all tests

---
+++ src/array-filter.test.js
@@ -0,0 +1,51 @@
+import { fiveAndGreaterOnly, evensOnly, fiveCharactersOrFewerOnly, peopleWhoBelongToTheIlluminati, ofAge } from './array-filter';
+
+test('Five and greater only', () => {
+  const input = [3, 6, 8, 2]; // arrange
+  const output = fiveAndGreaterOnly(input); // act
+  expect(output).toEqual([6, 8]); // assert
+});
+
+test('evens only', () => {
+  const input = [3, 6, 8, 2]; // arrange
+  const output = evensOnly(input); // act
+  expect(output).toEqual([6, 8, 2]); // assert
+});
+
+test('five chars or fewer only', () => {
+  const input = ['dog', 'wolf', 'by', 'family', 'eaten', 'camping']; // arrange
+  const output = fiveCharactersOrFewerOnly(input); // act
+  expect(output).toEqual(['dog', 'wolf', 'by', 'eaten']); // assert
+});
+
+test('people who belong in the illuminati', () => {
+  const input = [
+    { name: 'Angelina Jolie', member: true },
+    { name: 'Eric Jones', member: false },
+    { name: 'Paris Hilton', member: true },
+    { name: 'Kayne West', member: false },
+    { name: 'Bob Ziroll', member: true }
+  ]; 
+  const output = peopleWhoBelongToTheIlluminati(input);
+  expect(output).toEqual([
+    { name: 'Angelina Jolie', member: true }, 
+    { name: 'Paris Hilton', member: true }, 
+    { name: 'Bob Ziroll', member: true } 
+  ]); 
+});
+
+test('people old enough to see The Matrix', () => {
+  const input = [
+    { name: 'Angelina Jolie', age: 80 },
+    { name: 'Eric Jones', age: 2 },
+    { name: 'Paris Hilton', age: 5 },
+    { name: 'Kayne West', age: 16 },
+    { name: 'Bob Ziroll', age: 100 }
+  ];
+  const output = ofAge(input);
+  expect(output).toEqual([ 
+    { name: 'Angelina Jolie', age: 80 },
+    { name: 'Bob Ziroll', age: 100 } 
+  ]); 
+});
+
